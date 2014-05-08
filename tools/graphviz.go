15ca68a365ba0d9d33dadfb1048fbc68415ec2ec
Pau Fernández <pau.fernandez@upc.edu>
2014-05-08 23:29:12 +0200

    Invoco 'dot' desde graphviz

--- tools/graphviz.go
+++ tools/graphviz.go
@@ -6,6 +6,7 @@ import (
 	"fmt"
 	eimg "go-evoimage"
 	"os"
+	"os/exec"
 )
 
 var (
@@ -19,19 +20,38 @@ func main() {
 	scanner := bufio.NewScanner(os.Stdin)
 	curr := 1
 	for scanner.Scan() {
+		// Read expression from line
 		e, err := eimg.Read(scanner.Text())
 		if err != nil {
 			fmt.Printf("ERROR: %s\n", err)
 			continue
 		}
-		filename := fmt.Sprintf("graph%04d.dot", curr)
-		curr++
-		file, err := os.Create(filename)
+
+		// Write dot file
+		dotfile := fmt.Sprintf("img%04d.dot", curr)
+		file, err := os.Create(dotfile)
 		if err != nil {
-			fmt.Printf("ERROR: Cannot create '%s': %s", filename, err)
+			fmt.Printf("ERROR: Cannot create '%s': %s", dotfile, err)
 			os.Exit(1)
 		}
 		e.Graphviz(file)
 		file.Close()
+
+		// invoke dot
+		pngfile := fmt.Sprintf("img%04dg.png", curr)
+		dot := exec.Command("dot", "-Tpng", "-o", pngfile, dotfile)
+		if err := dot.Run(); err != nil {
+			fmt.Printf("ERROR: Cannot run 'dot -Tpng -o %s %s': %s",
+				pngfile, dotfile, err)
+		}
+
+		// remove dot file
+		if err := os.Remove(dotfile); err != nil {
+			fmt.Printf("ERROR: Cannot delete file '%s': %s", dotfile, err)
+		}
+
+		fmt.Println(e)
+
+		curr++
 	}
 }
